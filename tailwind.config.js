2ce3a03c42bc032e633b5ba5e06ef0672400e7bb
Railton L Rodrigues <railtonleal98@gmail.com>
2024-11-20 17:40:12 -0300

    HEY-9: creating the frontend of our form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import flowbite from 'flowbite/plugin';
+
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
+
     content: [
+        './node_modules/flowbite/**/*.js',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
@@ -17,5 +22,5 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, flowbite],
 };
